4131296543e4fb6c4009223833a5faaba972d52b
Wesley Horton <hortowe@ohsu.edu>
2018-12-17 16:16:38 -0800

    Update all ggplot themes so that they are loaded with function. Note that previously (when in utilityFxns.R), they're called like '+ my_theme', but now they will be '+ my_theme()'.

--- R/ggThemes.R
+++ R/ggThemes.R
@@ -4,16 +4,9 @@ library(ggplot2)
 ### Standard ggplot theme #######################################################################################################
 ###
 
-# my_theme <- theme_classic() +
-#   theme(plot.title = element_text(hjust = 0.5, size = 18),
-#         axis.text = element_text(size = 12),
-#         axis.title = element_text(size = 14),
-#         legend.text = element_text(size = 12),
-#         legend.title = element_text(size = 14))
-
 my_theme <- function() {
   #' Base Custom Theme
-  #' @description Minor changes to theme_classic()
+  #' @description Minor changes to theme_classic() - slightly larger text and centered title
   #' @export
   
   theme_classic() +
@@ -28,43 +21,81 @@ my_theme <- function() {
 ### Big Label  ggplot theme #####################################################################################################
 ###
 
-big_label <- theme_classic() +
-  theme(plot.title = element_text(hjust = 0.5, size = 20),
-        axis.text = element_text(size = 16),
-        axis.text.y = element_text(angle = 45),
-        axis.title = element_text(size = 18),
-        legend.text = element_text(size = 16),
-        legend.title = element_text(size = 18))
+big_label <- function() {
+  #' Big Label Theme
+  #' @description Same as my_theme(), but even larger text and also y-axis labels are angled 45
+  #' @export
+  
+  theme_classic() +
+    theme(plot.title = element_text(hjust = 0.5, size = 20),
+          axis.text = element_text(size = 16),
+          axis.text.y = element_text(angle = 45),
+          axis.title = element_text(size = 18),
+          legend.text = element_text(size = 16),
+          legend.title = element_text(size = 18))
+}
 
 ###
 ### Angle Text theme ############################################################################################################
 ###
 
-angle_x <- theme(axis.text.x = element_text(angle = 45, hjust = 1))
-angle_y <- theme(axis.text.y = element_text(angle = 45))
-angle_both <- theme(axis.text.x = element_text(angle = 45, hjust = 1),
-                    axis.text.y = element_text(angle = 45))
+angle_x <- function() {
+  #' Angle X Theme
+  #' @description Just add 45 degree adjustment to X axis
+  #' @export
+  
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+}
+
+angle_y <- function() {
+  #' Angle Y Theme
+  #' @description Just add 45 degree adjustment to Y axis
+  #' @export
+  
+  theme(axis.text.y = element_text(angle = 45))
+}
+
+angle_both <- function() {
+  #' Angle Both Theme
+  #' @description Add 45 degree adjustment to both axes
+  #' @export
+  
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        axis.text.y = element_text(angle = 45))
+}
 
 ###
 ### Times New Roman #############################################################################################################
 ###
 
-times <- my_theme +
-  theme(plot.title = element_text(family = "Times New Roman", hjust = 0.5, size = 18),
-        axis.text = element_text(family = "Times New Roman", size = 12),
-        axis.title = element_text(family = "Times New Roman", size = 14),
-        legend.text = element_text(family = "Times New Roman", size = 12),
-        legend.title = element_text(family = "Times New Roman", size = 12),
-        strip.text = element_text(family = "Times New Roman", size = 14))
+times <- function() {
+  #' Times New Roman Theme
+  #' @description Same as my_theme(), but font is all Times New Roman
+  #' @export
+  
+  my_theme +
+    theme(plot.title = element_text(family = "Times New Roman", hjust = 0.5, size = 18),
+          axis.text = element_text(family = "Times New Roman", size = 12),
+          axis.title = element_text(family = "Times New Roman", size = 14),
+          legend.text = element_text(family = "Times New Roman", size = 12),
+          legend.title = element_text(family = "Times New Roman", size = 12),
+          strip.text = element_text(family = "Times New Roman", size = 14))
+}
 
 ###
 ### Big Label Times New Roman ###################################################################################################
 ###
 
-bl_times <- my_theme +
-  theme(plot.title = element_text(family = "Times New Roman", hjust = 0.5, size = 20),
-        axis.text = element_text(family = "Times New Roman", size = 16),
-        axis.title = element_text(family = "Times New Roman", size = 18),
-        legend.text = element_text(family = "Times New Roman", size = 16),
-        legend.title = element_text(family = "Times New Roman", size = 18),
-        strip.text = element_text(family = "Times New Roman", size = 18))
+bl_times <- function() {
+  #' Big Label Times
+  #' @description Same as big_label(), but font is all Times New Roman
+  #' @export
+  
+  my_theme +
+    theme(plot.title = element_text(family = "Times New Roman", hjust = 0.5, size = 20),
+          axis.text = element_text(family = "Times New Roman", size = 16),
+          axis.title = element_text(family = "Times New Roman", size = 18),
+          legend.text = element_text(family = "Times New Roman", size = 16),
+          legend.title = element_text(family = "Times New Roman", size = 18),
+          strip.text = element_text(family = "Times New Roman", size = 18))
+}
