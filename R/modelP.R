39762c6d77a1e8fb2d2a2520ef95e2c663b608ae
Wesley Horton <hortowe@ohsu.edu>
2019-01-14 16:15:49 -0800

    Updated modelP so that the p-value from any variable in the model can be extracted (only for anova)

--- R/modelP.R
+++ R/modelP.R
@@ -1,7 +1,9 @@
-modelP <- function(model, round_v = T, verbose_v = F) {
+modelP <- function(model, var_v = NULL, round_v = NULL, verbose_v = F) {
   #' Extract p-value from model
   #' @description Extract the p-value estimate from either a linear model (lm) or ANOVA (aov) object.
   #' @param model model object of class 'lm' for linear model, classes 'lm' and 'aov' for ANOVA.
+  #' @param var_v character string of variable to get p-value from. If NULL (default) will take 1st.
+  #' @param round_v numeric. how many digits to round p-value output to. If NULL (default) will not round.
   #' @param round_v logical. TRUE - round p-value to 3 decimals; FALSE - do not round.
   #' @param verbose_v logical. TRUE - print model type; FALSE - don't print
   #' @return numeric vector of p-value
@@ -29,7 +31,9 @@ modelP <- function(model, round_v = T, verbose_v = F) {
   
   ## Get p-value
   if (class_v == "aov") {
-    p_v <- summary_v[[1]][["Pr(>F)"]][[1]]
+    summary_v <- summary_v[[1]]
+    which_v <- ifelse(is.null(var_v), 1, which(trimws(rownames(summary_v)) == var_v))
+    p_v <- summary_v[["Pr(>F)"]][[which_v]]
   } else {
     temp <- summary_v$fstatistic
     p_v <- pf(temp[1], temp[2], temp[3], lower.tail = F)
@@ -37,11 +41,14 @@ modelP <- function(model, round_v = T, verbose_v = F) {
   } # fi
   
   ## Round
-  p_v <- ifelse(round_v, round(p_v, digits = 3), p_v)
+  out_p_v <- ifelse(is.null(round_v), p_v, round(p_v, digits = round_v))
   
   ## If 0, make it < 2.2e-16
-  p_v <- ifelse(p_v == 0, p_v <- "< 2.2e-16", p_v)
+  if (out_p_v == 0 & p_v > 0) {
+    if (verbose_v) print("Rounded p-value is 0, so using unrounded p-value instead.")
+    out_p_v <- p_v
+  }
   
   ## Output
-  return(p_v)
+  return(out_p_v)
 } # modelP
