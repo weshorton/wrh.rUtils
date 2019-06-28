32aaac68820165506484d4a0564fb9e1ad47936c
Wesley Horton <hortowe@ohsu.edu>
2019-06-28 09:28:35 -0700

    Added a few checks to mergeAndFillCols. They're currently commented out, but may be useful if I run into problems in the future

--- R/mergeAndFillCols.R
+++ R/mergeAndFillCols.R
@@ -1,4 +1,4 @@
-mergeAndFillCols <- function(data_dt, toMerge_v, outName_v = 'merged', search_lsv = NULL, replace_lsv = NULL) {
+mergeAndFillCols <- function(data_dt, toMerge_v, outName_v = 'merged', search_lsv = NULL, replace_lsv = NULL, check_v = T) {
   #' Merge and Fill Columns
   #' @description Takes >= 2 columns with overlapping, incomplete data and merges them into 1 column.
   #' @param data_dt data.table with values
@@ -7,6 +7,7 @@ mergeAndFillCols <- function(data_dt, toMerge_v, outName_v = 'merged', search_ls
   #' @param search_lsv list of values to search for within the columns. This is done before merge. 
   #' One example is the 'age' columns that have KJ in them. Can search for those and replace with NA to take the value in the other column.
   #' @param replace_lsv list of values to replace. Each element must match with search_lsv
+  #' @param check_v logical. TRUE (default) check new column accuracy by searching for '_'; FALSE - don't check
   #' @export
   
   ## Search and replace
@@ -19,6 +20,14 @@ mergeAndFillCols <- function(data_dt, toMerge_v, outName_v = 'merged', search_ls
     })}), .SDcols = toMerge_v]
   } # fi
   
+  ## Change "NA" to NA - don't think this is needed either
+  #for (c_v in toMerge_v) data_dt[get(c_v) == "NA", (c_v) := NA]
+    
+  ## Get classes - I don't think this is helpful
+  # class_v <- unlist(sapply(toMerge_v, function(x) class(data_dt[[x]])))
+  # if (length(unique(unlist(class_v))) > 1) warning("Different classes. Trying first.")
+  # class_v <- class_v[[1]]
+
   ## Paste together
   data_dt[, newCol := do.call(paste, c(.SD, sep = "_")), .SDcols = toMerge_v]
   
@@ -27,6 +36,13 @@ mergeAndFillCols <- function(data_dt, toMerge_v, outName_v = 'merged', search_ls
   
   ## Remove original columns
   data_dt[, (toMerge_v) := NULL]
+  
+  ## If both merged columns had same value, need to just get the one
+  data_dt$newCol <- sapply(data_dt$newCol, function(x) {
+    y <- unique(strsplit(x, split = "_")[[1]])
+    yy <- paste(y, collapse = "_")
+    return(yy)
+  })
   
   ## Check new columns
   splitNewCol_v <- lapply(data_dt$newCol, function(x) strsplit(x, split = "_")[[1]])
