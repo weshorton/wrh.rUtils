7f588fef6102a3607c03c2237a30c3d74b4c01d9
Wesley Horton <hortowe@ohsu.edu>
2019-10-21 10:54:10 -0700

    Updated g_legend to check if ggplot object has a legend. If not, just print message instead of error.

--- R/extractGGLegend.R
+++ R/extractGGLegend.R
@@ -7,7 +7,11 @@ g_legend <- function(a.gplot){
   
   tmp <- ggplot_gtable(ggplot_build(a.gplot))
   leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
-  legend <- tmp$grobs[[leg]]
-  return(legend)
+  if (length(leg) > 0) {
+    legend <- tmp$grobs[[leg]]
+    return(legend)
+  } else {
+    message("No legend grob in provided ggplot")
+  }
 } # g_legend
 
