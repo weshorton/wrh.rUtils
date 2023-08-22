0ba6ddff2a4a2f9d4cc286934d41637ca9d87110
Wes Horton <hortowe@rnb11642.ohsu.edu>
2023-08-22 09:49:23 -0700

    Forgot to turn value to numeric

--- R/getOutliers.R
+++ R/getOutliers.R
@@ -168,7 +168,7 @@ getOutliers <- function(data_dt, col_v, type_v = c("mean", "mad", "dmad", "iqr")
       
       ### Get info
       currDir_v <- strsplit(lgt_v[i], split = ":")[[1]][1]
-      currVal_v <- strsplit(lgt_v[i], split = ":")[[1]][2]
+      currVal_v <- as.numeric(strsplit(lgt_v[i], split = ":")[[1]][2])
       currCol_v <- strsplit(lgt_v[i], split = ":")[[1]][3]
       currOutlierCol_v <- paste0(currCol_v, "Outlier")
       
