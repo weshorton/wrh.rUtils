29416574dd699487389da09dccb13f65b36027be
weshorton <wrhorton592@gmail.com>
2023-11-24 15:26:42 -0800

    Add secondary method to profitMarginCalculator. Still needs work. Method 1 is the default.

--- R/profitMarginCalculator.R
+++ R/profitMarginCalculator.R
@@ -1,5 +1,5 @@
 profitMarginCalculator <- function(price_v, margin_v = NULL, shippingWeight_v = 0, shop_v,
-                                   includeShippingInMargin_v = F) {
+                                   includeShippingInMargin_v = F, method_v = 1) {
   #' Profit Margin Calculator
   #' @description
     #' Calculate appropriate price depending on desired margin and potential shipping costs.
@@ -8,33 +8,58 @@ profitMarginCalculator <- function(price_v, margin_v = NULL, shippingWeight_v = 
   #' @param shippingWeight_v numeric value of shipping weight in pounds.
   #' @param shop_v either "PlantBoy" or "PlantShop" indicating which supplier
   #' @param includeShippingInMargin_v logical to add shipping cost before calculating margin
+  #' @param method_v either 1, meaning the original method, or 2, the 2x cost - shipping method.
   #' @details
     #' Use supplied info to calculate the appropriate price for a product.
   #' @return numeric vector of suggested price
   #' @export
   
+  ### Handle null
+  if (is.null(shippingWeight_v)) print("Waiting on shipping weight.")
+  #if (is.null(potSize_v)) print("Waiting on pot size.")
+  
   ### Get shipping price
-  shippingCost_v <- shippingCalculator(weight_v = shippingWeight_v, shop_v = shop_v)
+  shippingCost_v <- wrh.rUtils::shippingCalculator(weight_v = shippingWeight_v, shop_v = shop_v)
     
   ### Add to total cost
   if (includeShippingInMargin_v) {
     markup_v <- (price_v + shippingCost_v) * margin_v
+    paste_v <- "Markup ((price + shipping) * margin): %s\n"
   } else {
     markup_v <- price_v * margin_v
+    paste_v <- "Markup (price * margin): %s\n"
   } # fi
   
   markup_v <- round(markup_v, digits = 2)
   
-  # Final price
-  finalPrice_v <- round(price_v + shippingCost_v + markup_v, digits = 2)
+  if (method_v == 1) {
+    
+    # Final price
+    finalPrice_v <- round(price_v + shippingCost_v + markup_v, digits = 2)
+
+  } else if (method_v == 2) {
+    
+    # Final price
+    finalPrice_v <- round(2*price_v + shippingCost_v, digits = 2)
+    
+    # Backcalc markup
+    calculatedMarkup_v <- finalPrice_v - (price_v + shippingCost_v)
+    calculatedMargin_v <- round(calculatedMarkup_v / finalPrice_v, digits = 2)
+    
+    # Change
+    markup_v <- calculatedMarkup_v
+    margin_v <- calculatedMargin_v
+    
+  } # fi
+  
+  #if (is.null(potSize_v)) potSize_v <- "none"
   
   # Update
-  cat(sprintf("Item weight: %s\n
-              Shipping Cost: %s\n
-              Pre-markup price: %s\n
-              Price+shipping: %s\n
-              Markup: %s\n
-              Final Price: %s\n",
-              shippingWeight_v, shippingCost_v, price_v, (price_v+shippingCost_v), markup_v, finalPrice_v))
+  cat(sprintf(paste0("Wholesale price: %s\n
+    Shipping Weight: %s\n
+    Shipping Cost: %s\n",
+    paste_v,
+    "Sale Price (price + shipping + markup): %s\n
+    Margin:%s\n"), price_v, shippingWeight_v, shippingCost_v, markup_v, finalPrice_v, margin_v))
   
 } # profitMarginCalculator
