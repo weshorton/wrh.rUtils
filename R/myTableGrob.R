1debd3e18391ac84e5f40b105824c18a182ed0fd
weshorton <wrhorton592@gmail.com>
2024-08-22 16:39:46 -0700

    Update myTableGrob to have specific references to the external packages it uses

--- R/myTableGrob.R
+++ R/myTableGrob.R
@@ -13,10 +13,10 @@ myTableGrob <- function(data_dt, title_v, fontsize_v = 14){
   #' @export
   
   ## Table
-  table_grob <- tableGrob(data_dt, rows = rep('', nrow(data_dt)), theme = ttheme_minimal())
+  table_grob <- gridExtra::tableGrob(data_dt, rows = rep('', nrow(data_dt)), theme = gridExtra::ttheme_minimal())
   ## Title
-  title_grob <- textGrob(title_v, gp = gpar(fontsize = fontsize_v))
+  title_grob <- grid::textGrob(title_v, gp = grid::gpar(fontsize = fontsize_v))
   ## Add title
-  table_grob <- gtable_add_rows(table_grob, heights = grobHeight(title_grob) + unit(5,'mm'), pos = 0)
-  table_grob <- gtable_add_grob(table_grob, title_grob, 1, 1, 1, ncol(table_grob), clip = "off")
+  table_grob <- gtable::gtable_add_rows(table_grob, heights = grid::grobHeight(title_grob) + unit(5,'mm'), pos = 0)
+  table_grob <- gtable::gtable_add_grob(table_grob, title_grob, 1, 1, 1, ncol(table_grob), clip = "off")
 }
