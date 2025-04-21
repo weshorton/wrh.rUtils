25765b07acc747f641f073ece9164ddc1ea94aa2
weshorton <wrhorton592@gmail.com>
2025-04-21 10:39:13 -0700

    Add position argument for kable. Trying to use it to have multiple on one line, first test didn't work. Default with center is still the same as before

--- R/myKable.R
+++ R/myKable.R
@@ -1,18 +1,31 @@
-myKable <- function(data, width_v=100, ...) {
+myKable <- function(data, width_v=100, position_v="center", ...) {
   #' My Kable
   #' @description Function with a few common parameters I use already coded in
   #' @param data data.table/frame/matrix for plotting
   #' @param width number used as percentage of document width
+  #' @param position_v given to kable_styling position argument. Center is default. float_left, right, etc.
   #' @param ... extra arguments passed to knitr::kable
+  #' @details
+    #' Use "float_left" and "right" in conjunction to put multiple tables on the same line
+    #' 
   #' @return kable object
   #' @export
   
   ### Format width
   width_v <- paste0("style='width:", width_v, "%;'")
   
+  ### Handle position
+  if (position_v != "center") {
+    full_width_v <- T
+  } else {
+    full_width_v <- F
+  }
+  
   ### Make table
   myKable <- knitr::kable(data, format = "html", align = "c", table.attr = width_v, ...) %>%
-    kable_styling(position = "center", bootstrap_options = c("striped", "hover", "condensed"))
+    kable_styling(position = position_v, 
+                  full_width = full_width_v,
+                  bootstrap_options = c("striped", "hover", "condensed"))
   
   ### Output
   return(myKable)
