a5fe659e3aebe5c45dbf43d10018f9a04aa1ccb2
Wes Horton <wrhorton592@gmail.com>
2025-10-23 10:19:13 -0700

    Add format option

--- R/myKable.R
+++ R/myKable.R
@@ -1,7 +1,8 @@
-myKable <- function(data, width_v=100, position_v="center", ...) {
+myKable <- function(data, format_v = "html", width_v=100, position_v="center", ...) {
   #' My Kable
   #' @description Function with a few common parameters I use already coded in
   #' @param data data.table/frame/matrix for plotting
+  #' @param format_v argument to kable's `format`. Usually 'html' or 'latex', see ?knitr for others.
   #' @param width number used as percentage of document width
   #' @param position_v given to kable_styling position argument. Center is default. float_left, right, etc.
   #' @param ... extra arguments passed to knitr::kable
@@ -22,7 +23,7 @@ myKable <- function(data, width_v=100, position_v="center", ...) {
   }
   
   ### Make table
-  myKable <- knitr::kable(data, format = "html", align = "c", table.attr = width_v, ...) %>%
+  myKable <- knitr::kable(data, format = format_v, align = "c", table.attr = width_v, ...) %>%
     kable_styling(position = position_v, 
                   full_width = full_width_v,
                   bootstrap_options = c("striped", "hover", "condensed"))
