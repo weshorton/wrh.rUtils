66698cd9f0ca7a839697ebe6009a832c48e26a69
Wes Horton <wrhorton592@gmail.com>
2026-02-03 10:02:04 -0800

    Add title and axis break options

--- R/simpleSurvival.R
+++ R/simpleSurvival.R
@@ -1,5 +1,5 @@
-simpleSurvival <- function(data_dt, exprCol_v, timeCol_v = "OS.time", deathCol_v = "OS", method_v = "quartile", cutOff_v = NULL,
-                           outputFile_v = NULL, name_v = NULL, plotType_v = "gg", return_v = F, printPlot_v = T) {
+simpleSurvival <- function(data_dt, exprCol_v, timeCol_v = "OS.time", deathCol_v = "OS", method_v = "quartile", cutOff_v = NULL, break_v = NULL,
+                           outputFile_v = NULL, name_v = NULL, plotType_v = "gg", return_v = F, printPlot_v = T, title_v = "Survival") {
   #' Simple Survival
   #' @description
     #' Calculate Kaplan Meier survival estimate and plot. Split groups based on expression.
@@ -9,11 +9,13 @@ simpleSurvival <- function(data_dt, exprCol_v, timeCol_v = "OS.time", deathCol_v
   #' @param deathCol_v column that indicates death (vital) status. "DECEASED" == 1; "LIVING" == 0
   #' @param method_v which method to use to divide expression groups. quartile (default), median, or tertile.
   #' @param cutOff_v optional pre-calculated cut-off value to use to create groups (instead of calculating within function call)
+  #' @param breaks_v optional custom x-axis breaks
   #' @param outputFile_v optional path to an output location if saving file is desired
   #' @param name_v optional name to add to the title. Usually the title of the dataset used as input.
   #' @param plotType_v character vector. Either 'gg' or 'base' to indicate how to make the plot
   #' @param return_v logical indicating to return coxph and logrank test results (T) or not (F)
   #' @param printPlot_v logical indicating to output plot or not.
+  #' @param title_v prefix for plot title. Default is just 'Survival', could change to '5-year Survival' for example
   #' @details
     #' This function is specifically made for data that has been downloaded from the TCGA hub by the UCSCXenaTools package.
     #' [here](https://xenabrowser.net/datapages/?dataset=survival%2FBRCA_survival.txt&host=https%3A%2F%2Ftcga.xenahubs.net&removeHub=https%3A%2F%2Fxena.treehouse.gi.ucsc.edu%3A443) is an example of the BRCA
@@ -116,48 +118,60 @@ simpleSurvival <- function(data_dt, exprCol_v, timeCol_v = "OS.time", deathCol_v
   medians_v <- summary(fit)$table[,'median']
   names(medians_v) <- gsub("^.*=", "", names(medians_v))
   
+  ### Output
+  out_lsls <- list("cox" = list("pVal" = coxP_v, "result" = cox),
+                   "logRank" = list("pVal" = pval_v, "result" = survDiff))
+  
   ###
   ### Plot ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   ###
 
   ### Make title
   plotName_v <- ifelse(is.null(name_v), 
-                       paste0("Survival Based on ", exprCol_v, " ", simpleCap(method_v), " Expression"),
-                       paste0("Survival of ", name_v, " Based on\n", exprCol_v, " ", simpleCap(method_v), " Expression"))
+                       paste0(title_v, " Based on ", exprCol_v, " ", simpleCap(method_v), " Expression"),
+                       paste0(title_v, " of ", name_v, " Based on\n", exprCol_v, " ", simpleCap(method_v), " Expression"))
   
-  if (printPlot_v) {
+  if (plotType_v == "gg") {
     
-    if (plotType_v == "gg") {
+    plotName_v <- paste0(plotName_v, "\nMedian Surv - ", paste(paste(names(medians_v), medians_v, sep = ": "), collapse = "; "))
+    
+    surv_gg <- suppressWarnings(survminer::ggsurvplot(fit = survminer::surv_fit(survObj ~ survCol, data = data_dt),
+                                                      data = data_dt,
+                                                      pval = T,
+                                                      title = plotName_v,
+                                                      surv.median.line = "hv",
+                                                      ggtheme = my_theme(),
+                                                      risk.table = T,
+                                                      xlab = timeCol_v,
+                                                      legend.title = exprCol_v,
+                                                      legend.labs = labs_v,
+                                                      legend = c(0.8,0.8),
+                                                      palette = c("blue", "red"),
+                                                      linewidth = 1.5, censor.size = 10,
+                                                      break.x.by = break_v,
+                                                      xscale = 365.25))
+    
+    out_lsls[["plot"]] <- surv_gg
+    
+    ### Save to file
+    if (!is.null(outputFile_v)) {
       
-      plotName_v <- paste0(plotName_v, "\nMedian Surv - ", paste(paste(names(medians_v), medians_v, sep = ": "), collapse = "; "))
+      pdf(file = outputFile_v, onefile = F)
+      print(surv_gg)
+      dev.off()
       
-      surv_gg <- suppressWarnings(survminer::ggsurvplot(fit = survminer::surv_fit(survObj ~ survCol, data = data_dt),
-                                                        data = data_dt,
-                                                        pval = T,
-                                                        title = plotName_v,
-                                                        surv.median.line = "hv",
-                                                        ggtheme = my_theme(),
-                                                        risk.table = T,
-                                                        xlab = timeCol_v,
-                                                        legend.title = exprCol_v,
-                                                        legend.labs = labs_v,
-                                                        legend = c(0.8,0.8),
-                                                        palette = c("blue", "red")))
+    } # fi is.null(outputFile_v)
+    
+    ### Print
+    if (printPlot_v) {
       
-      if (!is.null(outputFile_v)) {
-        
-        pdf(file = outputFile_v, onefile = F)
-        print(surv_gg)
-        dev.off()
-        
-      } else {
-        
-        print(surv_gg)
-        
-      } # fi
+      print(surv_gg)
       
-    } else if (plotType_v == "base") {
-      
+    } # fi printPlot_v
+    
+  } else if (plotType_v == "base") {
+    
+    if (printPlot_v) {
       if (!is.null(outputFile_v)) pdf(file = outputFile_v)
       
       ## Base plot
@@ -179,22 +193,88 @@ simpleSurvival <- function(data_dt, exprCol_v, timeCol_v = "OS.time", deathCol_v
       legend(max_x, .5, legend = paste0("p.val = ", round(pval_v, digits = 4)), bty = "n", xjust=1, cex = 1.2)
       legend(max_x, 1, legend = c(paste0("Low = ", medians_v[["low"]]), paste0("High = ", medians_v[["up"]])),
              bty = "n", xjust = 1, col=c("blue", "red"), lwd=2,  cex = 1.2, title = "Median Survival")
-      # Not sure why I had this?
-      # legend(max_x, .73, legend = c(paste0("Low = ", records_v[["low"]]), paste0("High = ", records_v[["up"]])),
-      #        bty  = "n", xjust = 1, col=c("blue", "red"), lwd=2, cex=1.2, title = "N. Patients")
       
       if (!is.null(outputFile_v)) dev.off()
-      
-    } else {
-      
-      stop(sprintf("Only 'gg' and 'base' supported for plotType_v. %s provided.", plotType_v))
-      
-    } # fi plotType_v
-  } # fi printPlot_v
+    } # fi printPlot_v
+    
+  } else {
+    
+    stop(sprintf("Only 'gg' and 'base' supported for plotType_v. %s provided.", plotType_v))
+    
+  }
+  
+  # if (printPlot_v) {
+  #   
+  #   if (plotType_v == "gg") {
+  #     
+  #     plotName_v <- paste0(plotName_v, "\nMedian Surv - ", paste(paste(names(medians_v), medians_v, sep = ": "), collapse = "; "))
+  #     
+  #     surv_gg <- suppressWarnings(survminer::ggsurvplot(fit = survminer::surv_fit(survObj ~ survCol, data = data_dt),
+  #                                                       data = data_dt,
+  #                                                       pval = T,
+  #                                                       title = plotName_v,
+  #                                                       surv.median.line = "hv",
+  #                                                       ggtheme = my_theme(),
+  #                                                       risk.table = T,
+  #                                                       xlab = timeCol_v,
+  #                                                       legend.title = exprCol_v,
+  #                                                       legend.labs = labs_v,
+  #                                                       legend = c(0.8,0.8),
+  #                                                       palette = c("blue", "red")))
+  #     
+  #     out_lsls[["plot"]] <- surv_gg
+  #     
+  #     if (!is.null(outputFile_v)) {
+  #       
+  #       pdf(file = outputFile_v, onefile = F)
+  #       print(surv_gg)
+  #       dev.off()
+  #       
+  #     } else {
+  #       
+  #       print(surv_gg)
+  #       
+  #     } # fi
+  #     
+  #   } else if (plotType_v == "base") {
+  #     
+  #     if (!is.null(outputFile_v)) pdf(file = outputFile_v)
+  #     
+  #     ## Base plot
+  #     plot(fit, col=c("blue", "red"), frame = F, lwd = 2, 
+  #          main = plotName_v,
+  #          mark.time = T, cex.main = 2, cex.lab = 1.5, cex.axis = 1.2,
+  #          xlab = gsub("\\.", " ", timeCol_v), ylab = "Prop. Survival")
+  #     
+  #     ## Add median survival lines
+  #     lines(c(0,max(medians_v)),c(0.5,0.5), lty = "dashed")
+  #     mapply(function(x,y) lines(rep(x,2),c(0,0.5),col=y, lty="dashed"), medians_v, c("blue", "red"))
+  #     
+  #     ## Get number of records
+  #     records_v <- summary(fit)$table[,'records']
+  #     names(records_v) <- gsub("^.*=", "", names(records_v))
+  #     
+  #     ## Add legends
+  #     max_x <- max(data_dt[[timeCol_v]], na.rm = T) * 0.95
+  #     legend(max_x, .5, legend = paste0("p.val = ", round(pval_v, digits = 4)), bty = "n", xjust=1, cex = 1.2)
+  #     legend(max_x, 1, legend = c(paste0("Low = ", medians_v[["low"]]), paste0("High = ", medians_v[["up"]])),
+  #            bty = "n", xjust = 1, col=c("blue", "red"), lwd=2,  cex = 1.2, title = "Median Survival")
+  #     # Not sure why I had this?
+  #     # legend(max_x, .73, legend = c(paste0("Low = ", records_v[["low"]]), paste0("High = ", records_v[["up"]])),
+  #     #        bty  = "n", xjust = 1, col=c("blue", "red"), lwd=2, cex=1.2, title = "N. Patients")
+  #     
+  #     if (!is.null(outputFile_v)) dev.off()
+  #     
+  #   } else {
+  #     
+  #     stop(sprintf("Only 'gg' and 'base' supported for plotType_v. %s provided.", plotType_v))
+  #     
+  #   } # fi plotType_v
+  # } # fi printPlot_v
   
   ### Output
-  out_lsls <- list("cox" = list("pVal" = coxP_v, "result" = cox),
-                   "logRank" = list("pVal" = pval_v, "result" = survDiff))
+  # out_lsls <- list("cox" = list("pVal" = coxP_v, "result" = cox),
+  #                  "logRank" = list("pVal" = pval_v, "result" = survDiff))
   
   if (return_v) return(out_lsls)
   
