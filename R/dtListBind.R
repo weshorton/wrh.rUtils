4fed509d53950a38a6ff669b40528f082a170271
Wes Horton <wrhorton592@gmail.com>
2026-02-03 10:01:27 -0800

    Add option to put new column at the front

--- R/dtListBind.R
+++ R/dtListBind.R
@@ -1,10 +1,11 @@
-dtListBind <- function(list_lsdt, newName_v = "Name") {
+dtListBind <- function(list_lsdt, newName_v = "Name", front_v = T) {
   #' Data table list bind
   #' @description
     #' rbind a list of data.tables and also add the
     #' names of the list elements as a new column in each dt.
   #' @param list_lsdt named list of data.tables
   #' @param newName_v character vector that will be the new column name
+  #' @param front_v logical indicating if new column should be moved to the first column
   #' @return single data.table with one more column that input data.tables and as 
   #' many rows as all the input tables combined.
   #' @export
@@ -24,7 +25,13 @@ dtListBind <- function(list_lsdt, newName_v = "Name") {
     y[[newName_v]] <- x
     return(y)}, simplify = F, USE.NAMES = T)
   
+  out_dt <- do.call(rbind, list_lsdt)
+  
+  if (front_v) {
+    out_dt <- out_dt[,mget(c(newName_v, setdiff(colnames(out_dt), newName_v)))]
+  }
+  
   ### Combine and output
-  return(do.call(rbind, list_lsdt))
+  return(out_dt)
   
 } #dtListBind
